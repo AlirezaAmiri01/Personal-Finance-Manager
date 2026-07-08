c38c7ca280c9564b7ce476ed75484eded20a8b68
Alireza <a.amiri.pariyan.@gmail.com>
2026-07-08 13:36:39 +0330

    Add main

--- transaction.py
+++ transaction.py
@@ -8,3 +8,14 @@ class Transaction:
         self.amount = amount
         self.type = ""
         self.created_at = date.today()
+
+    def __str__(self):
+        created = self.created_at.strftime("%Y/%m/%m/%d")
+        return (
+            f"\nid          : {self.id}\n"
+            f"\ntitle       : {self.title}\n"
+            f"\namount      : {self.amount}\n"
+            f"\ntype        : {self.type}\n"
+            f"\ncreated_at  : {created}\n"
+
+        )
